daad27252bf094f126c9ab4d8f8aeebe9b1c33d3
David Morgan <git@davee.pro>
2017-09-15 22:14:35 +0100

    add syscall implementation for exit(), getuid/getpid and gettid()

---
+++ src/internal/vita/syscall_interface.h
@@ -0,0 +1,6 @@
+#ifndef SYSCALL_INTERFACE_H
+#define SYSCALL_INTERFACE_H
+
+const char *__lookup_syscall_name(int n);
+
+#endif // SYSCALL_INTERFACE_H
--- src/internal/vita/syscall_interp.c
+++ src/internal/vita/syscall_interp.c
@@ -1,3 +1,10 @@
+#include "syscall.h"
+#include "syscall_interface.h"
+
+#include <psp2/kernel/threadmgr.h>
+#include <psp2/kernel/processmgr.h>
+#include <psp2/kernel/clib.h>
+
 #include <errno.h>
 
 extern int __vita_external_syscall_interp(int n, int r1, int r2, int r3, int r4, int r5, int r6) __attribute__((weak));
@@ -6,9 +13,25 @@ int __vita_syscall_interp(int n, int r1, int r2, int r3, int r4, int r5, int r6)
 {
     switch (n)
     {
+    case SYS_exit:
+        return sceKernelExitDeleteThread(r1);
+    case SYS_exit_group:
+        return sceKernelExitProcess(r1);
+    case SYS_geteuid:
+    case SYS_getuid:
+    case SYS_getegid32:
+    case SYS_getpid:
+        return sceKernelGetProcessId();
+    case SYS_gettid:
+        return sceKernelGetThreadId();
+    case SYS_tkill:
+        sceClibPrintf("got tkill for: %i %i\n", r1, r2);
+        return 0;
     default:
         if (__vita_external_syscall_interp)
             return __vita_external_syscall_interp(n, r1, r2, r3, r4, r5, r6);
+        else
+            sceClibPrintf("musl: unhandled syscall called: %i: %s\n", n, __lookup_syscall_name(n));
     }
 
     return -ENOSYS;
